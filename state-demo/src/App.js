f714e230c74e64e46b6c1f6cce001ed3216b76e0
super-lin0 <3924578651@qq.com>
2019-06-12 21:57:20 +0800

    setState异步的思考

--- state-demo/src/App.js
+++ state-demo/src/App.js
@@ -14,7 +14,26 @@ class App extends Component {
     this.setState({ count: this.state.count + 1 });
     this.setState({ count: this.state.count + 1 });
     console.log(this.state);
+
+    // 让setState连续更新的方法
+    // 方法一：函数式用法
+    this.setState((prevState, props) => ({
+      count: prevState.count + 1
+    }));
+
+    // 方法二：把setState更新之后的逻辑封装到一个函数中，并作为第二个参数传给setState
+    // this.setState(updater, [callback])
+    this.setState({ count: this.state.count + 1 }, () => {
+      this.setState({ count: this.state.count + 1 }, () => {
+        this.setState({ count: this.state.count + 1 });
+      });
+    });
   };
+  // 扩充state
+  setStateAsync = state => {
+    return new Promise(resolve => this.setState(state, resolve));
+  };
+
   render() {
     return (
       <div className="App">
