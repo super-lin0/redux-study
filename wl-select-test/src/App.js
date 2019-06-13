436406302cc7e9ec82fcb6c07853051e1bf5bd24
super-lin0 <3924578651@qq.com>
2019-06-13 14:47:41 +0800

    学校选择框测试工程

---
+++ wl-select-test/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+import Index from "wl-select";
+
+function App() {
+  return (
+    <div className="App">
+      <Index />
+    </div>
+  );
+}
+
+export default App;
