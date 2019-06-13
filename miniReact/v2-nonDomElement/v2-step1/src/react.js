3dd96eae0a9934f4648cb116a489f3c19d28e95c
super-lin0 <3924578651@qq.com>
2019-06-13 21:11:05 +0800

    当组件为一个无状态组件的时候(函数)

---
+++ miniReact/v2-nonDomElement/v2-step1/src/react.js
@@ -0,0 +1,22 @@
+(() => {
+  function anElement(element, children) {
+    if (typeof element === "function") {
+      return element();
+    } else {
+      const anElement = document.createElement(element);
+      anElement.innerHTML = children.join(" ");
+      return anElement;
+    }
+  }
+  function createElement(el, prop, ...children) {
+    return anElement(el, children);
+  }
+  window.React = {
+    createElement
+  };
+  window.ReactDOM = {
+    render: (el, domEl) => {
+      domEl.appendChild(el);
+    }
+  };
+})();
