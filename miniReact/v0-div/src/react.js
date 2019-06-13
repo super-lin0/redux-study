fc05a382b0fa928956a844bf57ea99595d7c02a6
super-lin0 <3924578651@qq.com>
2019-06-13 20:56:34 +0800

    模拟react API

---
+++ miniReact/v0-div/src/react.js
@@ -0,0 +1,18 @@
+(() => {
+  function div(children) {
+    const aDiv = document.createElement("div");
+    aDiv.innerHTML = children.join(" ");
+    return aDiv;
+  }
+  function createElement(el, prop, ...children) {
+    return div(children);
+  }
+  window.React = {
+    createElement
+  };
+  window.ReactDOM = {
+    render: (el, domEl) => {
+      domEl.appendChild(el);
+    }
+  };
+})();
