1caa1f12503544fd633a7aceb480a1b929fd94ba
super-lin0 <3924578651@qq.com>
2019-07-15 20:31:44 +0800

    Add React组件父子组件通信

---
+++ chengmo/chapter-02/controlpanel-sumary/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import ControlPanel from "./controlPanel";
+
+ReactDOM.render(<ControlPanel />, document.getElementById("root"));
