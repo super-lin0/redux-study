f4cac32c8f86b940722b1d0b954d4b1e6b611c5d
super-lin0 <3924578651@qq.com>
2019-08-14 16:21:30 +0800

    Add react-router动态加载

---
+++ chengmo/chapter-11/react_router_chunked/build/precache-manifest.d342858adcfbea9d97d0ab462bc67627.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8afa630096097505751ff0be6eb3ea90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8dfa23f62989d315fa0",
+    "url": "/static/css/main.2f530c5b.chunk.css"
+  },
+  {
+    "revision": "3f27f6418aa9a501465d",
+    "url": "/static/js/0.chunk.js"
+  },
+  {
+    "revision": "0f0ce9c0f03c261ac747",
+    "url": "/static/js/1.chunk.js"
+  },
+  {
+    "revision": "690b8c7db68b1023c95f",
+    "url": "/static/js/2.chunk.js"
+  },
+  {
+    "revision": "a34b275e7ada6fc0514e",
+    "url": "/static/js/3.chunk.js"
+  },
+  {
+    "revision": "031a423afc8ea6ef37e3",
+    "url": "/static/js/bundle.js"
+  },
+  {
+    "revision": "a8dfa23f62989d315fa0",
+    "url": "/static/js/main.chunk.js"
+  }
+]);
--- chengmo/chapter-11/react_router_chunked/src/Router.js
+++ chengmo/chapter-11/react_router_chunked/src/Router.js
@@ -1,20 +1,26 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Loadable from "@loadable/component";
 
-import Home from "./pages/Home";
-import About from "./pages/About";
-import NotFound from "./pages/NotFound";
 import Header from "./layout";
+import MyLoadingComponent from "./components/loading";
+
+const AsyncHome = Loadable(() => import("./pages/Home"), {
+  fallback: MyLoadingComponent
+});
+
+const AsyncAbout = Loadable(() => import("./pages/About"));
+const AsyncNotfound = Loadable(() => import("./pages/NotFound"));
 
 const RouterCfg = () => (
   <Router>
     <Header />
     <Switch>
       {/* 默认链接 */}
-      <Route exact path="/" component={Home} />
-      <Route exact path="/home" component={Home} />
-      <Route exact path="/about" component={About} />
-      <Route exact path="*" component={NotFound} />
+      <Route exact path="/" component={AsyncHome} />
+      <Route exact path="/home" component={AsyncHome} />
+      <Route exact path="/about" component={AsyncAbout} />
+      <Route exact path="*" component={AsyncNotfound} />
     </Switch>
   </Router>
 );
